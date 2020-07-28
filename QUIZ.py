4b3f866cf5388d907bc6a120154f1cddb15deb39
Paul <isakura313@gmail.com>
2020-07-28 19:50:23 +0300

    asdasd

--- QUIZ.py
+++ QUIZ.py
@@ -4,3 +4,13 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 
 import player_class
+
+class Window:
+    def __init__(self, title='Main', width = 600, height=600):
+        self.width = 600
+        self.height = 600
+        self.size = f'{width}x{height}'
+        self.display = tk.Tk()
+        self.display.title(title)
+        self.display.geometry(self.size)
+        self.display.resizable(0,0)
