78717720c83658566d7978e1738be6ea82e712f7
Paul <isakura313@gmail.com>
2020-07-28 19:45:04 +0300

    Initial commit

---
+++ QUIZ.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+import player_class
---
+++ player_class.py
@@ -0,0 +1,6 @@
+class Player:
+    def __init__(self, name, age, gender, path_to_image):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.path_to_image = path_to_image
